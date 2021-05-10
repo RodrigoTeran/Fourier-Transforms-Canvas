7ed59cff20fe57dbdaa92598d9085616b77984dd
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2021-05-09 21:11:44 -0500

    feat: add comments to new function
    Added comments of steps to important math function.

--- src/hooks/math/index.tsx
+++ src/hooks/math/index.tsx
@@ -40,6 +40,43 @@ export const useCoordenates: Coordenates = () => {
     }
   };
 
+  const calculateCoefficients = () => {
+    /* 
+      -> f(t) = # + #i
+      -> delta_t = 0.01
+      -> HOW_MANY_VECTORS = 50
+
+      1. sumar todos los valores de 
+          sum_n = f(t) e^ (-2 n PI i t ) * delta_t
+
+          0 <= t <= "largest value of t"
+      2. luego sacar promedio dividiendo
+          c_n = sum_n / "largest value of t"
+
+      3. el c_n debería de estar en la forma
+          # + #i
+      4. repetir los pasos del (1-3) con los valores de n:
+          -HOW_MANY_VECTORS / 2 <= n <= HOW_MANY_VECTORS / 2   # o sea 101 veces
+
+      5. Poner todos los coeficientes ( c_n ) en un array de objetos:
+        [ 
+          ...,
+          {
+            frequency: n,
+            real: #,
+            imaginary: #
+          }
+        ]
+
+        -> siendo los valores de n asi: 0, 1, -1, 2, -2, 3, -3, ... n, -n
+    */
+  };
+
+  const renderVectors = () => {
+    /* 
+    */
+  };
+
   return [
     (): void => {
       combValuesOfTime();
